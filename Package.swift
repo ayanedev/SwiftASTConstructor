5563bc7188f4885d26cb4f225bb22cfe9b8709a7
Joe Hinkle <joehinkle11@gmail.com>
2021-06-01 11:13:26 -0600

    test

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftASTConstructor",
-            targets: ["SwiftASTConstructor"]),
+            targets: ["SwiftASTConstructor", "SwiftSyntaxTarget"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50400.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50400.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +24,7 @@ let package = Package(
            name: "SwiftASTConstructor",
            url: "https://github.com/App-Maker-Software/SwiftAST/releases/download/0.50400.0/SwiftASTConstructor.xcframework.zip",
            checksum: "0df93927d7b62a839d61803438c6fa4f4d0ce94b8639b1e0785e4c75c76abd39"
-       )
+       ),
+        .target(name: "SwiftSyntaxTarget", dependencies: ["SwiftSyntax"], path: nil)
     ]
 )
